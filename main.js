777407169f05fd2eb0a05abd2274f44daf1a25b6
kizktai <luv.dr2xxy@gmail.com>
2019-09-17 21:52:31 +0900

    1done

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 new Vue({
   el: "#app",
-  data() {
-    return {
-      now_rule: null,
-      now_stage: null
-    };
+  data: {
+    now_rule: "今のルール",
+    now_maps: "ステージ"
   },
   mounted() {
-    axios
-      .get("https://spla2.yuu26.com/gachi/now")
-      .then(response => (this.now_rule = response.data.result[0].rule));
+    axios.get("https://spla2.yuu26.com/gachi/now").then(response => {
+      (this.now_rule = response.data.result[0].rule),
+        (this.now_maps = response.data.result[0].maps);
+    });
   }
 });
-
-// (
-//     (this.now_stage = response.data.result[0].maps)
-//   )
